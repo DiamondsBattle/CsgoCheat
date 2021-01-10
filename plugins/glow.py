90468da5dd42e18aca8b6aa89e6efb9a99b6b6fb
Louis Jarraud <diamondsbattle@tutanota.com>
2021-01-10 21:52:11 +0100

    Glow update into single class

--- plugins/glow.py
+++ plugins/glow.py
@@ -3,35 +3,36 @@ import pymem.process
 import keyboard
 import win32gui
 
-dwEntityList = 0x4D5022C
-dwLocalPlayer = 0xD3BBEC
-m_iTeamNum = 0xF4
-dwGlowObjectManager = 0x5298070
-m_iGlowIndex = 0xA438
+class Glow:
+    def __init__(self):
+        self.dwEntityList = 0x4D5022C
+        self.dwLocalPlayer = 0xD3BBEC
+        self.m_iTeamNum = 0xF4
+        self.dwGlowObjectManager = 0x5298070
+        self.m_iGlowIndex = 0xA438
 
-def main():
-    pm = pymem.Pymem('csgo.exe')
-    client = pymem.process.module_from_name(pm.process_handle, 'client.dll').lpBaseOfDll
+        self.pm = pymem.Pymem('csgo.exe')
+        self.client = pymem.process.module_from_name(self.pm.process_handle, 'client.dll').lpBaseOfDll
 
-    while True:
-        glow_manager = pm.read_int(client + dwGlowObjectManager)
-        for i in range(1, 32):
-            entity = pm.read_int(client + dwEntityList + i * 0x10)
+        while True:
+            self.glow_manager = self.pm.read_int(self.client + self.dwGlowObjectManager)
+            for i in range(1, 32):
+                self.entity = self.pm.read_int(client + self.dwEntityList + i * 0x10)
 
-            if entity:
-                entity_team_id = pm.read_int(entity + m_iTeamNum)
-                entity_glow = pm.read_int(entity + m_iGlowIndex)
+                if self.entity:
+                    self.entity_team_id = self.pm.read_int(self.entity + self.m_iTeamNum)
+                    self.entity_glow = self.pm.read_int(self.entity + self.m_iGlowIndex)
 
-            if entity_team_id == 3:
-                pm.write_float(glow_manager + entity_glow * 0x38 + 0x4, float(1))
-                pm.write_float(glow_manager + entity_glow * 0x38 + 0x8, float(0))
-                pm.write_float(glow_manager + entity_glow * 0x38 + 0xC, float(0))
-                pm.write_float(glow_manager + entity_glow * 0x38 + 0x10, float(1))
-                pm.write_int(glow_manager + entity_glow * 0x38 + 0x24, 1)
+                if self.entity_team_id == 3:
+                    self.pm.write_float(self.glow_manager + self.entity_glow * 0x38 + 0x4, float(1))
+                    self.pm.write_float(self.glow_manager + self.entity_glow * 0x38 + 0x8, float(0))
+                    self.pm.write_float(self.glow_manager + self.entity_glow * 0x38 + 0xC, float(0))
+                    self.pm.write_float(self.glow_manager + self.entity_glow * 0x38 + 0x10, float(1))
+                    self.pm.write_int(self.glow_manager + self.entity_glow * 0x38 + 0x24, 1)
 
-            elif entity_team_id == 2:
-                pm.write_float(glow_manager + entity_glow * 0x38 + 0x4, float(0))
-                pm.write_float(glow_manager + entity_glow * 0x38 + 0x8, float(1))
-                pm.write_float(glow_manager + entity_glow * 0x38 + 0xC, float(0))
-                pm.write_float(glow_manager + entity_glow * 0x38 + 0x10, float(1))
-                pm.write_int(glow_manager + entity_glow * 0x38 + 0x24, 1)
+                elif self.entity_team_id == 2:
+                    self.pm.write_float(self.glow_manager + self.entity_glow * 0x38 + 0x4, float(0))
+                    self.pm.write_float(self.glow_manager + self.entity_glow * 0x38 + 0x8, float(1))
+                    self.pm.write_float(self.glow_manager + self.entity_glow * 0x38 + 0xC, float(0))
+                    self.pm.write_float(self.glow_manager + self.entity_glow * 0x38 + 0x10, float(1))
+                    self.pm.write_int(self.glow_manager + self.entity_glow * 0x38 + 0x24, 1)
